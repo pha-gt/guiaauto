c12e2fdad7f5c045f7fc350de81856f2b296ec1c
desarrollo <desarrollo@SISDSK02>
2022-12-08 11:46:26 -0600

    Titulo

--- guiaauto/resources/views/welcome.blade.php
+++ guiaauto/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    GUIA AUTO
                 </div>
 
                 <div class="links">
